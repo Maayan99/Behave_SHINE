661b5fa545c4ae49b7c39b371bb0ed4cef2c2d98
Yewei-Liu <2300012959@stu.pku.edu.cn>
2025-11-10 10:02:01 +0800

    20251110

--- metanetwork_family.py
+++ metanetwork_family.py
@@ -44,7 +44,7 @@ class MetanetworkTransformer(nn.Module):
         self.transformer_layers = nn.ModuleList([nn.TransformerEncoderLayer(**transformer_cfg.encoder_cfg) for _ in range(transformer_cfg.num_layers)])
         
         self.scale = nn.Parameter(torch.ones((1, self.num_layers, self.num_mem_token, 1)), requires_grad=True)
-        # self.bias = nn.Parameter(torch.zeros((1, self.num_layers, self.num_mem_token, self.hidden_size)), requires_grad=True)
+        self.bias = nn.Parameter(torch.zeros((1, self.num_layers, self.num_mem_token, self.hidden_size)), requires_grad=True)
 
     def forward(self, memory_states:torch.Tensor) -> dict:
         '''
@@ -57,7 +57,7 @@ class MetanetworkTransformer(nn.Module):
                 memory_states = self.transformer_layers[i](memory_states.transpose(1, 2).flatten(0, 1)).unflatten(0, (batch_size, self.num_mem_token)).transpose(1, 2) # exchange information among layers
             else:
                 memory_states = self.transformer_layers[i](memory_states.flatten(0, 1)).unflatten(0, (batch_size, self.num_layers)) # exchange information among tokens
-        memory_states = memory_states * self.scale#  + self.bias
+        memory_states = memory_states * self.scale  + self.bias
         return memory_states.flatten(1, -1)
 
 class Metanetwork(nn.Module):
