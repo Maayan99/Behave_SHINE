258e1f228f73face8ffd3576b9821ddc7e1adf5e
Yewei-Liu <2300012959@stu.pku.edu.cn>
2026-01-01 02:43:46 +0800

    20260101

--- scripts/Qwen3-8B/pretrain_group_4layer_8lora_128metalora.sh
+++ scripts/Qwen3-8B/pretrain_group_4layer_8lora_128metalora.sh
@@ -10,20 +10,20 @@
 #SBATCH -o metalora.out
 #SBATCH -e metalora.err
 
-NAME=tmp
+NAME=8gpu_8lora_128metalora_lr5e-5_grouppretrain_1170 # 4layer
 NUM_GPUS=8
-MASTER_PORT=18930      
+MASTER_PORT=18920       
 CONFIG_NAME="Qwen3-8B"       
-SOURCE=transmla
+SOURCE=grouptransmla
 TRAIN_BATCH_SIZE=1
 TEST_BATCH_SIZE=1
 GRADIENT_ACCUMULATION_STEPS=4
 USE_GRADIENT_CHECKPOINT=False
 RESUME_GLOBAL_STEP=latest   # -1: don't resume,   int: resume from global steps,  latest: resume from latest
 LEARNING_RATE=5e-5
+CONVERSATION_MAX_LEN=1170   # Extra base len: 0 Extra chat len per turn: 11
+CONTEXT_MAX_LEN=$((CONVERSATION_MAX_LEN - 9)) # $((CONVERSATION_MAX_LEN - 10))
 TYPE=transformer
-CONVERSATION_MAX_LEN=1180 # 1170 - 1210
-CONTEXT_MAX_LEN=1171
 NUM_LAYERS=4
 WARMUP_STEPS=200
 METHOD=rl
@@ -43,6 +43,29 @@ export OMP_NUM_THREADS=4
 export NCCL_DEBUG=WARN
 export TORCH_DISTRIBUTED_DEBUG=INFO
 
+python generate_group_idx.py  \
+    --config-name $CONFIG_NAME \
+    name=$NAME \
+    mode=pretrain \
+    data.source=$SOURCE \
+    data.train_batch_size=$TRAIN_BATCH_SIZE \
+    data.eval_batch_size=$TEST_BATCH_SIZE \
+    run.gradient_accumulation_steps=$GRADIENT_ACCUMULATION_STEPS \
+    run.use_gradient_checkpoint=$USE_GRADIENT_CHECKPOINT \
+    resume_global_step=$RESUME_GLOBAL_STEP \
+    optim.learning_rate=$LEARNING_RATE \
+    metanetwork.type=$TYPE \
+    data.conversation_max_length=$CONVERSATION_MAX_LEN \
+    data.context_max_length=$CONTEXT_MAX_LEN \
+    metanetwork.transformer_cfg.num_layers=$NUM_LAYERS \
+    optim.warmup_steps=$WARMUP_STEPS \
+    metanetwork.method=$METHOD \
+    model.lora_r=$LORA_R \
+    model.metalora_r=$METALORA_R \
+    > tmp_pretrain_$NAME.txt 2>&1
+
+wait
+
 nohup torchrun \
     --nproc_per_node=$NUM_GPUS \
     --nnodes=1 \
@@ -60,9 +83,9 @@ nohup torchrun \
     run.use_gradient_checkpoint=$USE_GRADIENT_CHECKPOINT \
     resume_global_step=$RESUME_GLOBAL_STEP \
     optim.learning_rate=$LEARNING_RATE \
+    metanetwork.type=$TYPE \
     data.conversation_max_length=$CONVERSATION_MAX_LEN \
     data.context_max_length=$CONTEXT_MAX_LEN \
-    metanetwork.type=$TYPE \
     metanetwork.transformer_cfg.num_layers=$NUM_LAYERS \
     optim.warmup_steps=$WARMUP_STEPS \
     metanetwork.method=$METHOD \
